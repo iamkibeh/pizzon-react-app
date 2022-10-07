d29b101289522cae340b40d1c46179eaf1eeae5a
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-10-07 13:51:29 +0300

    add reservation component

--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const Reservation = () => {
-  return <div>You are in the booking section</div>
+  return (
+    <>
+      You are in the booking section
+      <div className='reservation-cont'>
+        <div className='res-heading'>
+          <h2>make online reservation</h2>
+          <h1>Book a table</h1>
+        </div>
+        <ReservationForm />
+      </div>
+    </>
+  )
 }
 
 export default Reservation
