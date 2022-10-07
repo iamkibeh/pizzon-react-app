3bc5681e709da5c30b17a77504df3d3fc0e1fc97
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-10-07 13:52:15 +0300

    add image slideshow data

---
+++ src/Components/SliderData.js
@@ -0,0 +1,18 @@
+export const SliderData = [
+  {
+    image:
+      'https://images.pexels.com/photos/1395967/pexels-photo-1395967.jpeg?auto=compress&cs=tinysrgb&w=600',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/1833349/pexels-photo-1833349.jpeg?auto=compress&cs=tinysrgb&w=600',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/2253643/pexels-photo-2253643.jpeg?auto=compress&cs=tinysrgb&w=600',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/541216/pexels-photo-541216.jpeg?auto=compress&cs=tinysrgb&w=600',
+  },
+]
